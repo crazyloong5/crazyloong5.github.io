86ef58d63e8a0015007bfe2a44ad868ed2adca76
crazyloong5 <2603327054@qq.com>
2024-12-09 22:18:37 +0800

    Site updated: 2024-12-09 22:18:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2022/10/15/hello-world copy/","2024/12/09/hello-world/","2022/10/15/template/"];function toRandomPost(){
+var posts=["2022/10/15/template/","2022/10/15/hello-world copy/","2024/12/09/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
